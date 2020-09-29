20230142005ed08e876063f1372cd49406986f72
kyrptonaught <anthonybuttillo10@gmail.com>
2020-09-29 02:13:32 -0400

    Allow opening of stacked ender chests

--- src/main/java/net/kyrptonaught/quickshulker/api/Util.java
+++ src/main/java/net/kyrptonaught/quickshulker/api/Util.java
@@ -26,10 +26,10 @@ public class Util {
     }
 
     public static Boolean isOpenableItem(ItemStack stack) {
-        if (stack.getCount() != 1) return false;
         Item item = stack.getItem();
         if (!(item instanceof BlockItem)) return false;
         Block block = ((BlockItem) item).getBlock();
+        if (!(block instanceof EnderChestBlock) && stack.getCount() != 1) return false;
         return QuickOpenableRegistry.consumers.containsKey(block.getClass());
     }
 
