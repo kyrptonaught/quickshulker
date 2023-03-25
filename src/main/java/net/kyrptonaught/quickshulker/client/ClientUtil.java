6d41761a8e4e29c7cf3f7fe38b21020eb9fa836a
opekope2 <opekope2@gmail.com>
2023-03-25 15:26:55 +0100

    Remove unused code duplication

--- src/main/java/net/kyrptonaught/quickshulker/client/ClientUtil.java
+++ src/main/java/net/kyrptonaught/quickshulker/client/ClientUtil.java
@@ -9,12 +9,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
-import net.minecraft.util.Identifier;
-import opekope2.optifinecompat.properties.ChestProperties;
-import opekope2.optifinecompat.properties.OptiFineProperties;
-import opekope2.optifinecompat.properties.ShulkerBoxProperties;
-import opekope2.optigui.service.RegistryLookupService;
-import opekope2.optigui.service.Services;
 
 public class ClientUtil {
 
@@ -53,60 +47,5 @@ public class ClientUtil {
             }
         }
         return slot.getIndex();
-    }
-
-    public static Object getInteractionTargetData(ItemStack stack) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        RegistryLookupService lookup = Services.getService(RegistryLookupService.class);
-
-        String container = stack.getItem().toString();
-        Identifier biome = lookup.lookupBiome(client.world, client.player.getBlockPos());
-        String name = stack.hasCustomName() ? stack.getName().getString() : null;
-
-        return switch (container) {
-            case "shulker_box",
-                    "white_shulker_box",
-                    "orange_shulker_box",
-                    "magenta_shulker_box",
-                    "light_blue_shulker_box",
-                    "yellow_shulker_box",
-                    "lime_shulker_box",
-                    "pink_shulker_box",
-                    "gray_shulker_box",
-                    "light_gray_shulker_box",
-                    "cyan_shulker_box",
-                    "purple_shulker_box",
-                    "blue_shulker_box",
-                    "brown_shulker_box",
-                    "green_shulker_box",
-                    "red_shulker_box",
-                    "black_shulker_box" -> new ShulkerBoxProperties(
-                    name,
-                    biome,
-                    client.player.getBlockY(),
-                    getColorFromShulkerBox(container));
-            case "ender_chest" -> new ChestProperties(
-                    name,
-                    biome,
-                    client.player.getBlockY(),
-                    false,
-                    false,
-                    false,
-                    true,
-                    false,
-                    false);
-            case "crafting_table", "stonecutter" -> new OptiFineProperties(
-                    name,
-                    biome,
-                    client.player.getBlockY());
-            default -> null;
-        };
-    }
-
-    private static String getColorFromShulkerBox(String itemName) {
-        assert itemName.endsWith("shulker_box");
-
-        String color = itemName.substring(0, itemName.length() - "shulker_box".length());
-        return color.isEmpty() ? null : color.substring(0, color.length() - 1);
     }
 }
